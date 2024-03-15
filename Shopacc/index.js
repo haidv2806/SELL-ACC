9fcf3a0b9923d3eaa72637b48d30f93648175f82
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-03-15 18:50:42 +0700

    them funtion remove

--- Shopacc/index.js
+++ Shopacc/index.js
@@ -19,6 +19,10 @@ app.post("/add", async (req, res) =>{
  
 })
 
+app.post("/remove", async (req, res) =>{
+ 
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
