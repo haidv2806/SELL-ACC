56fb5dad9b0b97799630b4dc3f305201dab8c7dc
Đỗ Văn Hải <haidv28062004@gmail.com>
2024-03-20 17:38:50 +0700

    add databse code

--- Shopacc/index.js
+++ Shopacc/index.js
@@ -5,13 +5,51 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "Haidv2806",
+  port: 5432,
+});
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  //Write your code here.
-  res.render("index.ejs", {
+// let menuIMG = [];
+// let rac =[];
 
+// const result = await db.query('SELECT menuimg FROM menu', (err,res) =>{
+//   if (err) {
+//     console.error("Error executing query", err.stack);
+//   } else {
+//     menuIMG = res.rows;
+//     console.log(menuIMG);
+//     JSON.stringify(menuIMG);
+//     rac = menuIMG.menuimg;
+//     console.log(rac);
+//   }
+// })
+
+async function checkIMG() {
+  const result = await db.query("SELECT menuimg FROM menu");
+  let menuIMG = [];
+  let rac = [];
+  result.rows.forEach((img) => {
+    menuIMG.push(img.menuimg);
+  });
+  console.log(menuIMG);
+  return menuIMG;
+};
+
+
+
+
+app.get("/", async (req, res) => {
+  const menuIMG = await checkIMG();
+  res.render("index.ejs", {
+    menu_img: menuIMG,
   });
 });
 app.post("/add", async (req, res) =>{
